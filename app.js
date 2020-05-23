81ec7741c7889cafc308165fba2c0561957443ca
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-05-23 19:55:45 +0530

    created users endpoint

--- app.js
+++ app.js
@@ -1,45 +1,54 @@
-const createError = require("http-errors");
+// const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+// const path = require("path");
+// const cookieParser = require("cookie-parser");
+// const logger = require("morgan");
 const port = process.env.PORT || 3000;
 
-const indexRouter = require("./src/routes/index");
-const usersRouter = require("./src/routes/users");
+// const indexRouter = require("./src/routes/index");
+// const usersRouter = require("./src/routes/users");
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+// // view engine setup
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "pug");
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use("/", indexRouter);
+// // app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+//Creating REST API endpoints
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  res.send("testing");
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
+//Serving the app on port 3000
 app.listen(port, () => {
   console.log("server is up on port " + port);
 });
-module.exports = app;
+
+// module.exports = app;
