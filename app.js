7d208c46582ca0e6d916db81fa30d90796be3bca
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-05-23 19:30:41 +0530

    installed nodemon and serving app from app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const port = process.env.PORT || 3000;
 
-var indexRouter = require("./src/routes/index");
-var usersRouter = require("./src/routes/users");
+const indexRouter = require("./src/routes/index");
+const usersRouter = require("./src/routes/users");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", 'pug');
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -38,4 +39,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+app.listen(port, () => {
+  console.log("server is up on port " + port);
+});
 module.exports = app;
