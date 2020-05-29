a5a4b25318c5fe0349395dc82b1fdb97c12c9c01
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-05-29 21:10:08 +0530

    unique email needed for sign up

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -9,6 +9,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     lowercase: true,
+    unique: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error("Email is invalid");
@@ -25,8 +26,8 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   const user = this;
-  if (user.isModified("password")){
-    user.password = await bcrypt.hash(user.password, 8)
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
   }
   next();
 });
