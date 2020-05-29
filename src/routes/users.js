a5d3fa1b1527b7adcfe35de75a1828a9e7b294c6
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-05-29 21:23:43 +0530

    added endpoint to login users

--- src/routes/users.js
+++ src/routes/users.js
@@ -57,4 +57,18 @@ router.delete("/:id", async (req, res) => {
     res.status(500).send(e);
   }
 });
+
+//Endpoint for logging in user
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch {
+    res.status(404).send();
+  }
+});
 module.exports = router;
