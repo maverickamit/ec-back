10c348d3478017d6401fb222e14dced076f240f6
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-05-29 21:17:24 +0530

    added endpoint to get a list of all users

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const User = require("../db/models/user");
 
+//creating new user endpoint
 router.post("/", async (req, res) => {
   const user = new User(req.body);
   try {
@@ -12,14 +13,17 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.post("/login", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (err) {
-    res.status(400).send(err);
-  }
+// Reading User Endpoint
+//Getting a list of all Users
+
+router.get("/", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
 });
 
 module.exports = router;
