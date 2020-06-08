b8b3d9391ebb01e6d60c10a49d024a891f5006be
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-06-08 17:22:15 +0530

    fixed typo on user model

--- src/routes/users.js
+++ src/routes/users.js
@@ -20,7 +20,7 @@ router.post("/", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findByCredentials(
-      req.body.ema
+      req.body.email,
       req.body.password
     );
     const token = await user.generateAuthToken();
