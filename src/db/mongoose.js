d644239787f250cc59645e4e89eca1da0f9ad629
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-06-26 21:51:17 +0530

    added emailVerified field in database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb://Amit:ec123@cluster0-shard-00-00-aetxj.mongodb.net:27017,cluster0-shard-00-01-aetxj.mongodb.net:27017,cluster0-shard-00-02-aetxj.mongodb.net:27017/everchange-api?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+// mongoose.connect(
+//   "mongodb://Amit:ec123@cluster0-shard-00-00-aetxj.mongodb.net:27017,cluster0-shard-00-01-aetxj.mongodb.net:27017,cluster0-shard-00-02-aetxj.mongodb.net:27017/everchange-api?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
 
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
+//   {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//   }
+// );
+
+mongoose.connect("mongodb://localhost:27017/everchange-dev", {
+  useNewUrlParser: true,
+});
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { Binary } = require("mongodb");
 
 const userSchema = mongoose.Schema({
   firstName: { type: String, required: true, trim: true },
@@ -22,6 +23,11 @@ const userSchema = mongoose.Schema({
     required: true,
     trim: true,
     minlength: 8,
+  },
+  emailVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
   },
   tokens: [
     {
