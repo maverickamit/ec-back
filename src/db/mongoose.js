6caed15ea16bee3a77177d57e555e6f07cc64ebc
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2020-06-28 00:03:00 +0530

    updated for deployment

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-let prodDatabaseUrl = `mongodb+srv://Amit:ec123@cluster0-aetxj.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+let prodDatabaseUrl = `mongodb+srv://Amit:ec123@cluster0-aetxj.mongodb.net/everchange-api?retryWrites=true&w=majority`;
 let prodDatabaseUrl2 = `mongodb://Amit:ec123@cluster0-shard-00-00-aetxj.mongodb.net:27017,cluster0-shard-00-01-aetxj.mongodb.net:27017,cluster0-shard-00-02-aetxj.mongodb.net:27017/everchange-api?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
 let devDatabaseUrl = `mongodb://localhost:27017/everchange-dev`;
 
