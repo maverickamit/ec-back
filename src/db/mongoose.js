9443cdd7e51356e001fe40376bdf6a1b8fb95142
Amit <29959138+maverickamit@users.noreply.github.com>
2020-08-07 17:27:29 +0530

    updated database url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ let prodDatabaseUrl = `mongodb+srv://Amit:ec123@cluster0-aetxj.mongodb.net/everc
 let prodDatabaseUrl2 = `mongodb://Amit:ec123@cluster0-shard-00-00-aetxj.mongodb.net:27017,cluster0-shard-00-01-aetxj.mongodb.net:27017,cluster0-shard-00-02-aetxj.mongodb.net:27017/everchange-api?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`;
 let devDatabaseUrl = `mongodb://localhost:27017/everchange-dev`;
 
-mongoose.connect(devDatabaseUrl, {
+mongoose.connect(prodDatabaseUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
