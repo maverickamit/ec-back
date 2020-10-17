b8c4deb0b0678862af787640dba5832e5d33f197
Amit <29959138+maverickamit@users.noreply.github.com>
2020-10-18 00:57:55 +0530

    sending password reset form after successful token verification

--- src/routes/users.js
+++ src/routes/users.js
@@ -305,6 +305,7 @@ router.post("/forgotPassword", async (req, res) => {
     res.status(500).send();
   }
 });
+
 //Endpoint for handling password reset url
 router.get("/forgotPassword/reset/:id/:token", async (req, res) => {
   try {
@@ -316,7 +317,73 @@ router.get("/forgotPassword/reset/:id/:token", async (req, res) => {
     const resetToken = jwt.decode(
       req.params.token, secret
     );
-    res.send();
+    res.send(
+      `<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+      <style>html,body { height: 100%; }
+      
+        body{
+          display: -ms-flexbox;
+          display: -webkit-box;
+          display: flex;
+          -ms-flex-align: center;
+          -ms-flex-pack: center;
+          -webkit-box-align: center;
+          align-items: center;
+          -webkit-box-pack: center;
+          justify-content: center;
+          background-color: #f5f5f5;
+        }
+        
+        form{
+          padding-top: 10px;
+          font-size: 14px;
+          margin-top: 30px;
+        }
+        
+        .card-title{ font-weight:300; }
+        
+        .btn{
+          font-size: 14px;
+          margin-top:20px;
+        }
+        
+        .login-form{ 
+          width:320px;
+          margin:20px;
+        }
+        
+        .sign-up{
+          text-align:center;
+          padding:20px 0 0;
+        }
+        
+        span{
+          font-size:14px;
+        }</style>
+      
+        <div class="card login-form">
+        <div class="card-body">
+          <h3 class="card-title text-center">Reset password</h3>
+          
+          <div class="card-text">
+            <form action="/users/forgotPassword/reset" method="POST"
+            oninput='pw2.setCustomValidity(pw2.value != pw.value ? "Passwords do not match." : "");
+            pw.setCustomValidity(pw.value.length <8 ? "Please increase length of your password" : "")'>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Enter new password</label>
+                <input type="password" class="form-control form-control-sm" required name=pw placeholder="Greater than or equal to 8 characters">
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Retype new password</label>
+                <input type="password" class="form-control form-control-sm" placeholder="" name=pw2>
+              </div>
+              <button type="submit" class="btn btn-primary btn-block">Reset</button>
+            </form>
+          </div>
+        </div>
+      </div>`
+);
   } catch(e) {
     res.status(500)
     .send(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
