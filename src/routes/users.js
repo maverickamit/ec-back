c7c57813f5f27ffa79df941b3d5270be6739fd20
Amit <29959138+maverickamit@users.noreply.github.com>
2020-12-14 14:53:14 +0530

    updated styling for password reset successful/error message

--- src/routes/users.js
+++ src/routes/users.js
@@ -515,25 +515,193 @@ router.post("/forgotPassword/reset", async (req, res) => {
     await user.save();
     sendSuccessfulResetEmail(user.email, user.firstName);
 
-    res.send(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <div className="container">
-      <div className="row">
-        <p class="font-weight-bold " style="padding:20px;font-size:calc(100% + 1vh)" >Password changed successfully.</p>
-        <p  style="padding-left:20px;font-size:calc(100% + 1vh);">Please click 
-        <a href="https://everchange.herokuapp.com">here</a> 
-      to continue to login.</p>
+    res.send(`<html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <script
+        src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"
+      ></script>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous"
+      />
+  
+      <style>
+        html,
+        body {
+          height: 100%;
+        }
+  
+        nav {
+          margin-bottom: 20px;
+          min-height: 80px;
+        }
+        .nav-link,
+        .navbar-brand {
+          color: #007bff !important;
+        }
+        .message {
+          padding-bottom: 50px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" href="https://everchange.herokuapp.com/"
+            >EverChange</a
+          >
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="https://everchange.herokuapp.com/"
+                  >Login <span class="sr-only">(current)</span></a
+                >
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  href="https://everchange.herokuapp.com/registration"
+                  >Sign Up</a
+                >
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-    </div>`);
+      <div class="container bg-light">
+        <div className="row ">
+          <p class="font-weight-bold" style="padding: 10px">
+            Password changed successfully.
+          </p>
+          <p class="message" style="padding-left: 10px">
+            Please click
+            <a href="https://everchange.herokuapp.com">here</a>
+            to continue to login.
+          </p>
+        </div>
+      </div>
+    </body>
+  </html>`);
   } catch {
-    res.status(500)
-      .send(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <div className="container">
-      <div className="row">
-        <p class="font-weight-bold " style="padding:20px;font-size:calc(100% + 1vh)" >There has been an error.</p>
-        <p  style="padding-left:20px;font-size:calc(100% + 1vh);">Please retry again 
-        <a href="https://everchange.herokuapp.com/forgot-password">here</a>.</p>
-      </div>
-    </div>`);
+    res.status(500).send(`<html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+          crossorigin="anonymous"
+        ></script>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous"
+        />
+    
+        <style>
+          html,
+          body {
+            height: 100%;
+          }
+    
+          nav {
+            margin-bottom: 20px;
+            min-height: 80px;
+          }
+          .nav-link,
+          .navbar-brand {
+            color: #007bff !important;
+          }
+          .message {
+            padding-bottom: 50px;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container bg-light">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="https://everchange.herokuapp.com/"
+              >EverChange</a
+            >
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="https://everchange.herokuapp.com/"
+                    >Login <span class="sr-only">(current)</span></a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    href="https://everchange.herokuapp.com/registration"
+                    >Sign Up</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div class="container bg-light">
+          <div className="row ">
+            <p class="font-weight-bold" style="padding: 10px">
+              There has been an error.
+            </p>
+            <p class="message" style="padding-left: 10px">
+              Please retry again
+              <a href="https://everchange.herokuapp.com/forgot-password">here</a>.
+            </p>
+          </div>
+        </div>
+      </body>
+    </html>
+    `);
   }
 });
 
