61c8a61f7c157feda0051faa322cedb9ff296a66
Amit <29959138+maverickamit@users.noreply.github.com>
2021-02-20 21:12:35 +0530

    fix: moved from email to env variable

--- src/emails/account.js
+++ src/emails/account.js
@@ -8,7 +8,7 @@ sgMail.setApiKey(SENDGRID_KEY);
 const sendWelcomeEmail = (email, name, token) => {
   sgMail.send({
     to: email,
-    from: "amitdgpghosh@gmail.com",
+    from: process.env.FROM_EMAIL,
     subject: "Welcome to EverChange " + name,
     html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml"><head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
